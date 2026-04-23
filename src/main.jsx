0ec4f6125a2e3d2b1292c47813255c5a920ff4f3
Saran R K <saran@example.com>
2026-04-23 13:10:45 +0530

    Remove admin dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import AdminDashboard from './AdminDashboard'
 import './index.css'
-
-// Determine which component to show based on current path
-const isAdminPath = window.location.pathname === '/admin';
-const ComponentToRender = isAdminPath ? AdminDashboard : App;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ComponentToRender />
+    <App />
   </React.StrictMode>,
 )
